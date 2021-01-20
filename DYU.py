33180cbfc521a26d80336de8074d9228b98f3d92
Muhammed Tarık Demir <73182570+str1g1f0rmes@users.noreply.github.com>
2021-01-20 21:05:05 +0300

    DYU.py Geliştirme.
    Katılmayan öğrencilerin alt alta yazdırılma özelliği getirildi.

--- DYU.py
+++ DYU.py
@@ -28,13 +28,20 @@ Yoklama_1 = Yoklama.iloc[:,0]
 
 Katılmayanlar = list(set(SınıfL_1)-set(Yoklama_1))
 
-
 katılmayanlar_i = input("Katılmayanlar Listesinin Adı Ne Olsun?: ")
 
+
 yoklama = Workbook()
-sheet = yoklama.active
-sheet.append(Katılmayanlar)
+katılmayanlarl = yoklama.active
+katılmayanlarl.column_dimensions['A'].width = 35
+
+for i in range(0,len(Katılmayanlar)):
+            
+            katılmayanlarh=katılmayanlarl.cell(row=i+1, column=1)
+            katılmayanlarh.value=Katılmayanlar[i]
+            
 yoklama.save(katılmayanlar_i)
 yoklama.close()
+
 
 print("\nİşlem Gerçekleştirildi, Kolay Gelsin.")
